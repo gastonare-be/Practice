a71dddf008f26f53c0047674d8ddf96464c2a19f
gastonare <gastonare@hotmail.com>
2020-09-09 16:10:36 -0300

    Add project files.

---
+++ NewYearChaos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Solution
+{
+
+    // Complete the minimumBribes function below.
+    static int minimumBribes(int[] q)
+    {
+        var number = q.Length;
+
+        bool samePerson = false;
+        int totalCount = 0;
+        int currentCount = 0;
+
+        for (int i = q.Length - 1; i >= 0; i--)
+        {
+            if (q[i] <= number)
+            {
+                samePerson = true;
+                totalCount++;
+                currentCount++;
+
+                if (samePerson && currentCount > 2)
+                {
+                    return -1;
+                }
+
+            }
+
+            else
+            {
+                currentCount = 0;
+                samePerson = false;
+            }
+
+            number--;
+
+        }
+
+        return currentCount + totalCount;
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
+            ;
+            var bribes = minimumBribes(q);
+
+            if (bribes == -1)
+            {
+                Console.WriteLine("Too chaotic");
+            }
+
+            else
+            {
+                Console.WriteLine(bribes);
+            }
+        }
+    }
+}
---
+++ Palindrome/PalindromePermutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindrome
+{
+    public class PalindromePermutation
+    {
+        public int checkPalindrome(string word)
+        {
+            Dictionary<int, string> words = new Dictionary<int, string>();
+
+            var sum = 0;
+            var lenght = words.Count;
+
+            words.Add(0, "c");
+            words.Add(1, "i");
+            words.Add(2, "v");
+            words.Add(3, "i");
+            words.Add(4, "c");
+
+            foreach (var item in words)
+            {
+                if (item.Value[0] == item.Value[4] && item.Value[1] == item.Value[3])
+                {
+                    return 1;
+                }
+            }
+
+            return sum;
+        }
+
+    };
+}
